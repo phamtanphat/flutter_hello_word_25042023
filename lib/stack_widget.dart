cbfbbd58444e49b144f10008e61ff48e3665cb82
phamtanphat <phatdroid94@gmail.com>
2023-05-30 18:39:30 +0700

    clip behavior

--- lib/stack_widget.dart
+++ lib/stack_widget.dart
@@ -11,19 +11,29 @@ class _StackWidgetState extends State<StackWidget> {
   @override
   Widget build(BuildContext context) {
     return Stack(
+      alignment: Alignment.center,
+      clipBehavior: Clip.none,
       children: [
-        Align(
-          alignment: Alignment.topRight,
-          child: Container(color: Colors.red, width: 100, height: 100),
-        ),
-        Align(
-          alignment: Alignment.bottomLeft,
-          child: Container(color: Colors.blue, width: 100, height: 100),
-        ),
-        Positioned(
-          left: 10,
-          top: 10,
-          child: Container(color: Colors.orange, width: 100, height: 100),
+        Container(
+          decoration: BoxDecoration(
+            border: Border.all(),
+            borderRadius: BorderRadius.circular(10)
+          ),
+          width: 200,
+          height: 200,
+          child: Stack(
+            clipBehavior: Clip.none,
+            children: [
+              Align(
+                alignment: Alignment.topRight,
+                child: Container(color: Colors.red, width: 100, height: 500),
+              ),
+              Positioned(
+                left: -50,
+                child: Container(color: Colors.blue, width: 100, height: 100),
+              ),
+            ],
+          ),
         )
       ],
     );
